f57e72cf66fbe81f0e1e894f3515af3bd45091e2
guoyangqiao <944734785@qq.com>
2019-07-15 16:04:06 +0800

    let's try on elastic join

---
+++ elasticsearch/src/main/java/org/apache/calcite/adapter/elasticsearch/ElasticsearchJoin.java
@@ -0,0 +1,35 @@
+package org.apache.calcite.adapter.elasticsearch;
+
+import org.apache.calcite.plan.RelOptCluster;
+import org.apache.calcite.plan.RelOptCost;
+import org.apache.calcite.plan.RelOptPlanner;
+import org.apache.calcite.plan.RelTraitSet;
+import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.rel.core.CorrelationId;
+import org.apache.calcite.rel.core.Join;
+import org.apache.calcite.rel.core.JoinRelType;
+import org.apache.calcite.rel.metadata.RelMetadataQuery;
+import org.apache.calcite.rex.RexNode;
+
+import java.util.Set;
+
+public class ElasticsearchJoin extends Join implements ElasticsearchRel {
+
+    protected ElasticsearchJoin(RelOptCluster cluster, RelTraitSet traitSet, RelNode left, RelNode right, RexNode condition, Set<CorrelationId> variablesSet, JoinRelType joinType) {
+        super(cluster, traitSet, left, right, condition, variablesSet, joinType);
+    }
+
+    @Override
+    public void implement(Implementor implementor) {
+    }
+
+    @Override
+    public Join copy(RelTraitSet traitSet, RexNode conditionExpr, RelNode left, RelNode right, JoinRelType joinType, boolean semiJoinDone) {
+        return null;
+    }
+
+    @Override
+    public RelOptCost computeSelfCost(RelOptPlanner planner, RelMetadataQuery mq) {
+        return super.computeSelfCost(planner, mq).multiplyBy(0.1);
+    }
+}
