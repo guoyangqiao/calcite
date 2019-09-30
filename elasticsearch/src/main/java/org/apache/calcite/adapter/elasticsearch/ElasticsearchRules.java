6150ba41e6e33b992dac1ccc234ad23db7b91585
guoyangqiao <944734785@qq.com>
2019-10-01 01:31:16 +0800

    autocommit

--- elasticsearch/src/main/java/org/apache/calcite/adapter/elasticsearch/ElasticsearchRules.java
+++ elasticsearch/src/main/java/org/apache/calcite/adapter/elasticsearch/ElasticsearchRules.java
@@ -335,7 +335,7 @@ class ElasticsearchRules {
                 final String matchStr = conditionGroup.stream().map(x -> {
                   final RexNode rexNode = ((RexCall) x).getOperands().get(1);
                   if (rexNode instanceof RexLiteral) {
-                    return ElasticsearchConstants.trimPercentSign(String.valueOf(((RexLiteral) rexNode).getValue()));
+                    return ElasticsearchConstants.trimPercentSign(((RexLiteral) rexNode).getValueAs(String.class));
                   }
                   throw new IllegalArgumentException("Not compatible with non literal");
                 }).collect(Collectors.joining(ElasticsearchConstants.WHITE_SPACE));
