64806fae837ba2941999959c2cb38bee9a1dfdd7
guoyangqiao <944734785@qq.com>
2019-12-20 21:29:16 +0800

    edit MATCHES_ANY

--- core/src/main/java/org/apache/calcite/sql/fun/SqlMatchesAnyFunction.java
+++ core/src/main/java/org/apache/calcite/sql/fun/SqlMatchesAnyFunction.java
@@ -1,10 +1,12 @@
 package org.apache.calcite.sql.fun;
 
 import org.apache.calcite.rel.type.RelDataTypeFactory;
-import org.apache.calcite.sql.SqlFunction;
-import org.apache.calcite.sql.SqlFunctionCategory;
-import org.apache.calcite.sql.SqlKind;
+import org.apache.calcite.sql.*;
 import org.apache.calcite.sql.type.SqlReturnTypeInference;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class SqlMatchesAnyFunction extends SqlFunction {
 
@@ -21,7 +23,50 @@ public class SqlMatchesAnyFunction extends SqlFunction {
         SqlKind.OTHER_FUNCTION,
         RETURN_TYPE_INFERENCE,
         null,//TODO
-        null,//TODO
-        SqlFunctionCategory.USER_DEFINED_SPECIFIC_FUNCTION);
+        null,
+        SqlFunctionCategory.SYSTEM);
+  }
+
+  @Override
+  public boolean checkOperandTypes(SqlCallBinding callBinding, boolean throwOnFailure) {
+    boolean ok = true;
+    Set<Class<? extends SqlNode>> valueType = new HashSet<>();
+    List<SqlNode> operands = callBinding.operands();
+    for (int i = 1; i < operands.size(); i++) {
+      Class<? extends SqlNode> valClazz = operands.get(i).getClass();
+      valueType.add(valClazz);
+    }
+    if (valueType.size() > 1) {
+      ok = false;
+    }
+    SqlNode sqlNode = operands.get(0);
+    if (!(sqlNode instanceof SqlIdentifier)) {
+      ok = false;
+    }
+    if (!ok && throwOnFailure) {
+      throw callBinding.newValidationSignatureError();
+    } else {
+      return ok;
+    }
+  }
+
+  @Override
+  public SqlOperandCountRange getOperandCountRange() {
+    return new SqlOperandCountRange() {
+      @Override
+      public boolean isValidCount(int count) {
+        return true;
+      }
+
+      @Override
+      public int getMin() {
+        return 2;
+      }
+
+      @Override
+      public int getMax() {
+        return Integer.MAX_VALUE;
+      }
+    };
   }
 }
